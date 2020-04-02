47bced5e4c06a821e00296e429d863a3dfe5ef7f
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-02 23:06:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ new_array = []
 while row_index < src.length do
   element_index = 0 
   while element_index < src[row_index].length do
-    if src[row_index][element_index]%2 = 0 
+    if src[row_index][element_index]%2 == 0 
       new_array << src[row_index][element_index]
     end
       element_index += 1
